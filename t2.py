3551ef2f8b9a95e1e9717bcd55eef3a5ea61f5a0
unknown <nmzfrank@nmzfrank.apexlab.org>
2015-08-05 15:39:31 +0800

    successfully get cookie for weibo login in t2.py

---
+++ t2.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+import httplib
+import urllib
+from StringIO import StringIO
+import gzip
+import re
+
+
+class login(object):
+	"""docstring for login"""
+	def __init__(self):
+		return 
+		
+
+
+	def prefetch(self):
+		addressPattern = re.compile('action="(?P<address>.+?)"')
+		vkPattern = re.compile('name="vk" value="(?P<vk>.+?)"')
+		pwPattern = re.compile('type="password" name="(?P<pwname>.+?)"')
+		self.headers = {
+			'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36',
+			'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+			'Accept-Encoding':'gzip, deflate',
+			'Accept-Language':'zh-CN,zh;q=0.8',
+			'Connection':'keep-alive',	
+			'Content-Type':'application/x-www-form-urlencoded',
+		}
+		self.conn = httplib.HTTPSConnection("login.weibo.cn")
+		self.conn.request("GET","https://login.weibo.cn/login/")
+		self.response = self.conn.getresponse()
+		self.cookie = self.response.getheader("Set-Cookie")
+		self.cookie = self.cookie.split(';',1)[0]
+		data = self.response.read()
+		address = addressPattern.search(data).group('address')
+		address = re.sub('&amp;','&',address)
+		address = re.sub("vt=1","vt=4",address)
+		self.pwname = pwPattern.search(data).group('pwname')
+		self.vk = vkPattern.search(data).group('vk')
+		self.redirect = "https://login.weibo.cn/login/" + address
+
+	def fetch(self):
+		self.prefetch()
+		addressPattern = re.compile('action="(?P<address>.+?)"')
+		vkPattern = re.compile('name="vk" value="(?P<vk>.+?)"')
+		pwPattern = re.compile('type="password" name="(?P<pwname>.+?)"')
+		data = {	
+				'mobile':'nmz110@126.com',
+				self.pwname:'happyending',
+				'remember':'on',
+				'backURL':'http%253A%252F%252Fweibo.cn',
+				'backTitle':'%E6%89%8B%E6%9C%BA%E6%96%B0%E6%B5%AA%E7%BD%91',
+				'tryCount':'',
+				'vk':str(self.vk),
+				'submit':'%E7%99%BB%E5%BD%95'
+		}
+		header = {	'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36',
+				'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				'Accept-Encoding':'gzip, deflate',
+				'Accept-Language':'zh-CN,zh;q=0.8',
+				'Connection':'keep-alive',	
+				'Cache-Control':'max-age=0',
+				'Content-Type':'application/x-www-form-urlencoded',				
+				'Host':'login.weibo.cn',
+				'Origin':'https://login.weibo.cn',
+				'Referer':'https://login.weibo.cn/login/?',
+				'Cookie': str(self.cookie),
+				'Upgrade-Insecure-Requests':1
+			}
+		data = urllib.urlencode(data)
+		self.conn.request("POST",self.redirect,data, header)
+		response = self.conn.getresponse()
+		print response.status
+		data = response.read()
+		buff = gzip.GzipFile(fileobj=StringIO(data))
+		data = buff.read()
+		print data
+		return
+
+httplib.HTTPConnection.debuglevel = 1
+
+l = login()
+l.fetch()
+
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
 import sys 
 import re
 import urllib3
+from urllib3 import Retry
 import BeautifulSoup
 
 def prefetch():
@@ -9,20 +11,48 @@ def prefetch():
 	content = r.data
 	soup = BeautifulSoup.BeautifulSoup(content)
 	formParamPattern = re.compile('action="(?P<address>.+?)"')
+	vkParamPattern = re.compile('name="vk" value="(?P<vk>.+?)"')
 	form = soup.body.renderContents()
-	match = formParamPattern.search(form)
-	return match.group('address')
+	address = formParamPattern.search(form)
+	vk = vkParamPattern.search(form)
+	return address.group('address'), vk.group('vk')
 
 
 def fetch():
-	address = prefetch()
-	t_address = 'http"//weibo.cn/login/' + address
+	address, vk = prefetch()
+	t_address = 'http://weibo.cn/login/' + address
+	print t_address,vk
 	http = urllib3.PoolManager()
-	header = {}
-	param = {}
-	r = http.request('POST', t_address, param, header)
+	header = {	'User-Agent':'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36',
+				'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+				'Accept-Encoding':'gzip, deflate',
+				'Accept-Language':'zh-CN,zh;q=0.8',
+				'Cache-Control':'max-age=0',
+				'Connection':'keep-alive',
+				'Content-Length':'191',
+				'Content-Type':'application/x-www-form-urlencoded',
+				'Cookie':'_T_WM=6935b80b7dfc8944210d3c536f2d3a97',
+				'Host':'login.weibo.cn',
+				'Origin':'https://login.weibo.cn',
+				'Referer':'https://login.weibo.cn/login/?ns=1&revalid=2&backURL=http%3A%2F%2Fweibo.cn%2F&backTitle=%CE%A2%B2%A9&vt=',
+				'Upgrade-Insecure-Requests':'1',
+			}
+	param = {	
+				'mobile':'nmz110@126.com',
+				'password_5647':'happyending',
+				'remember':'on',
+				'backURL':'http%3A%2F%2Fweibo.cn%2F',
+				'backTitle':u'微博',
+				'tryCount':'',
+				'vk':vk,
+				'submit':u'登录',
+		}
+	r = http.request('POST', t_address, param, header, retries = False)
 	content = r.data
-	return
+	print r.get_redirect_location()
+	r = http.request('POST', t_address, param, header, retries = False)
+	print r.getheader('location')
+	return content
 
 
-fetch()
+print fetch()
